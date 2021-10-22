4c55481930a2aaa4c99cfe85272ff1e3383af634
MeliOrtiz333 <91299508+MeliOrtiz333@users.noreply.github.com>
2021-10-22 09:01:51 -0500

    Add files via upload

---
+++ Ortiz_Melisa_Examen/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace Ortiz_Melisa_Examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Encabezado del programa y nombre
+            Console.WriteLine("Programa realizado por: Melisa Ortiz Morante (0221077)\n");
+            Console.WriteLine("Ingenieria Mecatronica\n");
+            Console.WriteLine("Programa que solicita nombre y ID del alumno, asi como un alnalisis de la descomposicion del ID\n");
+
+
+            //Variables
+            string nombre;
+            int ID;
+            int max = 100000;
+            int min = 399999;
+            int programa;
+            int num1;
+            int num2;
+            int num3;
+            int mayor;
+            int menor;
+            int suma;
+
+
+            //Pedir variables
+            //Pedir nombre
+            Console.Write("Hola, ¿Cual es tu nombre?:  ");
+            nombre = Convert.ToString(Console.ReadLine()); 
+            Console.WriteLine("Su nombre es:  " + nombre);
+
+            //Generarle un ID al usuario
+            Random generadorAleatorio = new Random();
+            ID = generadorAleatorio.Next(100000, 399999);
+            Console.WriteLine("Su ID es: \n" + ID);
+
+
+
+            //Menu para que el usario seleccione
+            Console.WriteLine("Primero seleccione que desea analizar del ID \n");
+
+            //Menu
+            Console.WriteLine("Presione {1} para descomoner el ID en tres partes\n");
+            Console.WriteLine("Presione {2} para sumar las tres partes en las que se divide el ID\n");
+            Console.WriteLine("Presione {3} para determinar cual es el mayor y cual es el menor\n");
+            Console.WriteLine("Presione {4} para ver el rango de menor a mayor de los tres numeros");
+            programa = Convert.ToInt32(Console.ReadLine());
+
+
+            //Ciclo para eleccion de lo que se busca analizar
+            switch (programa )
+            {
+                case 1: //Parte 1: Descomposicion en tres partes
+                    Console.WriteLine("Programa que descompone el Id en tres partes\n");
+
+                    num1 = (ID / 10000);
+                    num2 = (ID / 100) %100;
+                    num3 = (ID % 100);
+
+                    Console.WriteLine("La descomposicion en tres partes es " + num1 + "... " + num2 + "... y " + num3 + "");
+
+                   break;
+
+                case 2: //PArte 2: Suma de los tres numeros a base de la descomposicion anterior
+                    num1 = (ID / 10000);
+                    num2 = (ID / 100) % 100;
+                    num3 = (ID % 100);
+
+                    suma = num1 + num2 + num3;
+                    Console.WriteLine("La suma de la descomposicion en tres numeros del ID es: " + suma);
+
+                    break;
+
+                case 3: //Parte 3:Determinar cual de los dos tres numeros es mayor.
+
+                    num1 = (ID / 10000);
+                    num2 = (ID / 100) % 100;
+                    num3 = (ID % 100);
+
+
+                    if (num1 > num2)
+                    {
+                        mayor =num1 ;
+                       
+                        Console.WriteLine("El numero mayor es: ....." + mayor);
+                    }
+                    else if (num3> num1)
+                    {
+                        mayor = num3;
+
+                        Console.WriteLine("El numero mayor es: ....." + mayor);
+
+                    }
+                    if (num2 >num3 )
+                    {
+
+                        menor = num3;
+                    
+                        Console.WriteLine("El numero menor es:...." + menor);
+                        
+                    }
+
+                    else if (num3>num2)
+                    {
+                        mayor = num3;
+                        Console.WriteLine("El numero mayor es: ....." + mayor);
+
+
+                    }
+
+                         break;
+
+                case 4: //Rango ascendente
+                    num1 = (ID / 10000);
+                    num2 = (ID / 100) % 100;
+                    num3 = (ID % 100);
+
+                   
+
+
+                    break;
+
+            }
+
+
+
+
+
+            //Espera
+            Console.WriteLine("Programa exitoso, oprima <enter> para salir.....");
+            string espera = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Ortiz_Melisa_Smile/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+
+namespace E_SmileReloaded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables
+            int random_x;
+            int random_y;
+            int random;
+
+            // Crear caracteres y leer las teclas
+            ConsoleKeyInfo tecla;
+            char smile = '☻';
+            int max_ancho = 40; 
+            int max_alto = 17;
+            Console.SetCursorPosition(1, 1);
+            Console.Write("Smile");
+            int x = 2;
+            int y = 2;
+            Console.SetCursorPosition(x, y);
+
+            Console.Write(smile);
+
+            do
+            {
+
+                tecla = Console.ReadKey(false);
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        if (x <= max_ancho)
+                        {
+                            x++;
+                            Console.Write(' ');
+                            Console.SetCursorPosition(x, y);
+                            Console.Write(smile);
+                            Console.SetCursorPosition(85, 15);
+                            Console.Write("" + x + ", " + y + "");
+                            Console.SetCursorPosition(85, 15);
+                            Console.Write("Derecha →");
+                            Console.SetCursorPosition(x, y);
+                        }
+                        break;
+
+
+
+                    case ConsoleKey.LeftArrow:
+                        if (x > 1)
+                        {
+                            x--;
+                            Console.Write (' ');
+                            Console.SetCursorPosition(x, y);
+                            Console.Write(smile);
+                            Console.SetCursorPosition(85, 15);
+                            Console.Write("" + x + ", " + y + "");
+                            Console.SetCursorPosition(85, 15);
+                            Console.Write("Izquierda ←");
+                            Console.SetCursorPosition(x, y);
+
+                        }
+                        break;
+
+
+                    case ConsoleKey.DownArrow:
+                        if (y <= max_alto)
+                        {
+                            y++;
+                            Console.Write (' ');
+                            Console.SetCursorPosition(x, y);
+                            Console.Write(smile);
+                            Console.SetCursorPosition(85, 15);
+                            Console.Write("" + x + ", " + y + "");
+                            Console.SetCursorPosition(85, 15);
+                            Console.Write("Abajo ↓");
+                            Console.SetCursorPosition(x, y);
+
+                        }
+                        break;
+
+
+                    case ConsoleKey.UpArrow:
+                        if (y > 1)
+                        {
+                            y--;
+                            Console.Write (' ');
+                            Console.SetCursorPosition(x, y);
+                            Console.Write(smile);
+                            Console.SetCursorPosition(85, 15);
+                            Console.Write("" + x + ", " + y + "");
+                            Console.SetCursorPosition(85, 15);
+                            Console.Write("Arriba ↑");
+                            Console.SetCursorPosition(x, y);
+
+                        }
+                        break;
+
+
+
+                    case ConsoleKey.Tab:
+                        Console.Write(' ');
+                        Random genAleatoriox = new Random();
+                        Random genAleatorioy = new Random();
+
+                        random_x = genAleatoriox.Next(2, 39);
+                        random_y = genAleatorioy.Next(2, 16);
+                        x = random_x;
+                        y = random_y;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(smile);
+                        Console.SetCursorPosition(85, 15);
+                        Console.Write("" + x + ", " + y + "");
+                        Console.SetCursorPosition(85, 15);
+                        Console.Write("Aleatorio");
+                        Console.SetCursorPosition(x, y);
+                        break;
+
+                    //Generar los aleatorios.
+
+                    case ConsoleKey.Escape:
+                        Random genAleatoriod = new Random();
+
+                        random = genAleatoriod.Next(0, 5);
+
+                        switch (random)
+                        {
+                            case 1:
+                                if (x <= max_ancho)
+                                {
+                                    x++;
+                                    Console.Write(' ');
+                                    Console.SetCursorPosition(x, y);
+                                    Console.Write(smile);
+                                    Console.SetCursorPosition(85, 8);
+                                    Console.Write("" + x + ", " + y + "");
+                                    Console.SetCursorPosition(85, 12);
+                                    Console.Write("Derecha →");
+
+                                }
+                                break;
+
+                            case 2:
+                                if (x > 1)
+                                {
+                                    x--;
+                                    Console.Write(' ');
+                                    Console.SetCursorPosition(x, y);
+                                    Console.Write(smile);
+                                    Console.SetCursorPosition(85, 8);
+                                    Console.Write("" + x + ", " + y + "");
+                                    Console.SetCursorPosition(85, 12);
+                                    Console.Write("Izquierda ←");
+
+
+                                }
+                                break;
+
+                            case 3:
+                                if (y <= max_alto)
+                                {
+                                    y++;
+                                    Console.Write (' ');
+                                    Console.SetCursorPosition(x, y);
+                                    Console.Write(smile);
+                                    Console.SetCursorPosition(85, 8);
+                                    Console.Write(""+x+", "+ y+"");
+                                    Console.SetCursorPosition(85, 12);
+                                    Console.Write("Abajo ↓");
+
+
+                                }
+                                break;
+
+                            case 4:
+                                if (y > 1)
+                                {
+                                    y--;
+                                    Console.Write ( ' ');
+                                    Console.SetCursorPosition(x, y);
+                                    Console.Write(smile);
+                                    Console.SetCursorPosition(85, 8);
+                                    Console.Write("" + x + ", " + y + "");
+                                    Console.SetCursorPosition(85, 12);
+                                    Console.Write("Arriba ↑");
+
+
+                                }
+                                break;
+                        }
+                        break;
+                    case ConsoleKey.Spacebar:
+                        Console.Write(' ');
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(smile);
+                        Console.SetCursorPosition(85, 8);
+                        Console.Write("" + x + ", " + y + "");
+                        Console.SetCursorPosition(85, 12);
+                        Console.Write("Finalizado");
+                        Console.SetCursorPosition(x, y);
+                        break;
+                }
+            }
+            while (true);
+        }
+    }
+}
